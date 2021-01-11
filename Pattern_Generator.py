811555719cc6235a4221542e107172feba0edb15
CODEJIN <codejin2@gmail.com>
2021-01-12 07:01:14 +0900

    Wrong midi is skipped.

--- Pattern_Generator.py
+++ Pattern_Generator.py
@@ -22,7 +22,7 @@ def Pattern_Generate(
 
     paths = []
     for root, _, files in os.walk(dataset_path):
-        for file in files:
+        for file in sorted(files):
             if os.path.splitext(file)[1] != '.wav':
                 continue
             wav_Path = os.path.join(root, file).replace('\\', '/')
@@ -30,109 +30,113 @@ def Pattern_Generate(
             paths.append((wav_Path, midi_Path))
 
     for index, (wav_Path, midi_Path) in enumerate(paths):
-            mid = mido.MidiFile(midi_Path, charset='CP949')
-            midi_Length = sum([msg.time for msg in mid if msg.type != 'marker']) * hyper_paramters.Sound.Sample_Rate
-            music = []
-            current_Lyric = ''
-            previous_Used = 0
-            absolute_Position = 0
-            for message in mid:
-                if not message.type in ['note_on', 'lyrics', 'note_off']:   # Removing end maker.
+        mid = mido.MidiFile(midi_Path, charset='CP949')
+        midi_Length = sum([msg.time for msg in mid if msg.type != 'marker']) * hyper_paramters.Sound.Sample_Rate
+        music = []
+        current_Lyric = ''
+        previous_Used = 0
+        absolute_Position = 0
+        wrong_Midi = False
+        for message in mid:
+            if not message.type in ['note_on', 'lyrics', 'note_off']:   # Removing end maker.
+                continue
+            if message.type == 'note_on':
+                duration = int(message.time * hyper_paramters.Sound.Sample_Rate) + previous_Used
+                previous_Used = duration % hyper_paramters.Sound.Frame_Shift
+                duration = duration // hyper_paramters.Sound.Frame_Shift
+                music.append((absolute_Position, duration, '<X>', 0))
+                absolute_Position += duration
+            elif message.type == 'lyrics':
+                current_Lyric = Decompose(message.text.strip())
+            elif message.type == 'note_off':
+                duration = int(message.time * hyper_paramters.Sound.Sample_Rate) + previous_Used
+                previous_Used = duration % hyper_paramters.Sound.Frame_Shift
+                duration = duration // hyper_paramters.Sound.Frame_Shift
+                music.append((absolute_Position, 2, current_Lyric[0], message.note))   # Onset
+                absolute_Position += 2
+                music.append((absolute_Position, duration - 4, current_Lyric[1], message.note))  # excepting onset and coda length
+                absolute_Position += duration - 4
+                music.append((absolute_Position, 2, current_Lyric[2], message.note))   # Coda
+                absolute_Position += 2
+
+                if duration - 4 < 2:    # I want nucleus is also longer than 2.
+                    print('\nToo short note. This data is skipped. Please check it: {}, {}'.format(wav_Path, midi_Path))
+                    wrong_Midi = True
+                    break
+        if wrong_Midi:
+            continue
+
+        audio = Audio_Prep(wav_Path, hyper_paramters.Sound.Sample_Rate)[:int(midi_Length)]  # trimming additional silence of end point
+        mel = Mel_Generate(
+            audio,
+            sample_rate= hyper_paramters.Sound.Sample_Rate,
+            num_mel= hyper_paramters.Sound.Mel_Dim,
+            num_frequency= hyper_paramters.Sound.Spectrogram_Dim,
+            window_length= hyper_paramters.Sound.Frame_Length,
+            hop_length= hyper_paramters.Sound.Frame_Shift,
+            mel_fmin= hyper_paramters.Sound.Mel_F_Min,
+            mel_fmax= hyper_paramters.Sound.Mel_F_Max,
+            max_abs_value= hyper_paramters.Sound.Max_Abs_Mel
+            )[:absolute_Position]   # Usually, 1 or 2 step of mel is cut.
+
+        pitch = pitch_calc(
+            sig= audio,
+            sr= hyper_paramters.Sound.Sample_Rate,
+            w_len= hyper_paramters.Sound.Frame_Length,
+            w_step= hyper_paramters.Sound.Frame_Shift,
+            f0_min= hyper_paramters.Sound.F0_Min,
+            f0_max= hyper_paramters.Sound.F0_Max,
+            confidence_threshold= hyper_paramters.Sound.Confidence_Threshold,
+            gaussian_smoothing_sigma = hyper_paramters.Sound.Gaussian_Smoothing_Sigma
+            )[:absolute_Position] / hyper_paramters.Sound.F0_Max
+
+        silence = np.where(np.mean(mel, axis=1) < -3.5, np.zeros_like(np.mean(mel, axis=1)), np.ones_like(np.mean(mel, axis=1)))
+
+        pattern_Index = 0
+        for start_Index in tqdm(range(len(music)), desc= os.path.basename(wav_Path)):
+            for end_Index in range(start_Index + 1, len(music), 3):
+                music_Sample = music[start_Index:end_Index]
+                sample_Length = music_Sample[-1][0] + music_Sample[-1][1] - music_Sample[0][0]
+                if sample_Length < hyper_paramters.Min_Duration:
                     continue
-                if message.type == 'note_on':
-                    duration = int(message.time * hyper_paramters.Sound.Sample_Rate) + previous_Used
-                    previous_Used = duration % hyper_paramters.Sound.Frame_Shift
-                    duration = duration // hyper_paramters.Sound.Frame_Shift
-                    music.append((absolute_Position, duration, '<X>', 0))
-                    absolute_Position += duration
-                elif message.type == 'lyrics':
-                    current_Lyric = Decompose(message.text.strip())
-                elif message.type == 'note_off':
-                    duration = int(message.time * hyper_paramters.Sound.Sample_Rate) + previous_Used
-                    previous_Used = duration % hyper_paramters.Sound.Frame_Shift
-                    duration = duration // hyper_paramters.Sound.Frame_Shift
-                    music.append((absolute_Position, 2, current_Lyric[0], message.note))   # Onset
-                    absolute_Position += 2
-                    music.append((absolute_Position, duration - 4, current_Lyric[1], message.note))  # excepting onset and coda length
-                    absolute_Position += duration - 4
-                    music.append((absolute_Position, 2, current_Lyric[2], message.note))   # Coda
-                    absolute_Position += 2
+                elif sample_Length > hyper_paramters.Max_Duration:
+                    break
 
-                    if duration - 4 < 2:    # I want nucleus is also longer than 2.
-                        raise Exception('Too short note. Please check it: {}, {}', format(wav_Path, midi_Path   ))
+                audio_Sample = audio[music_Sample[0][0] * hyper_paramters.Sound.Frame_Shift:(music_Sample[-1][0] + music_Sample[-1][1]) * hyper_paramters.Sound.Frame_Shift]
+                mel_Sample = mel[music_Sample[0][0]:music_Sample[-1][0] + music_Sample[-1][1]]
+                silence_Sample = silence[music_Sample[0][0]:music_Sample[-1][0] + music_Sample[-1][1]]
+                pitch_Sample = pitch[music_Sample[0][0]:music_Sample[-1][0] + music_Sample[-1][1]]
 
+                _, duration_Sample, text_Sample, Note_Sample = zip(*music_Sample)
 
-            audio = Audio_Prep(wav_Path, hyper_paramters.Sound.Sample_Rate)[:int(midi_Length)]  # trimming additional silence of end point
-            mel = Mel_Generate(
-                audio,
-                sample_rate= hyper_paramters.Sound.Sample_Rate,
-                num_mel= hyper_paramters.Sound.Mel_Dim,
-                num_frequency= hyper_paramters.Sound.Spectrogram_Dim,
-                window_length= hyper_paramters.Sound.Frame_Length,
-                hop_length= hyper_paramters.Sound.Frame_Shift,
-                mel_fmin= hyper_paramters.Sound.Mel_F_Min,
-                mel_fmax= hyper_paramters.Sound.Mel_F_Max,
-                max_abs_value= hyper_paramters.Sound.Max_Abs_Mel
-                )[:absolute_Position]   # Usually, 1 or 2 step of mel is cut.
+                pattern = {
+                    'Audio': audio_Sample.astype(np.float32),
+                    'Mel': mel_Sample.astype(np.float32),
+                    'Silence': silence_Sample.astype(np.uint8),
+                    'Pitch': pitch_Sample.astype(np.float32),
+                    'Duration': duration_Sample,
+                    'Text': text_Sample,
+                    'Note': Note_Sample,
+                    'Singer': 'Female_0',
+                    'Dataset': 'NAMS',
+                    }
 
-            pitch = pitch_calc(
-                sig= audio,
-                sr= hyper_paramters.Sound.Sample_Rate,
-                w_len= hyper_paramters.Sound.Frame_Length,
-                w_step= hyper_paramters.Sound.Frame_Shift,
-                f0_min= hyper_paramters.Sound.F0_Min,
-                f0_max= hyper_paramters.Sound.F0_Max,
-                confidence_threshold= hyper_paramters.Sound.Confidence_Threshold,
-                gaussian_smoothing_sigma = hyper_paramters.Sound.Gaussian_Smoothing_Sigma
-                )[:absolute_Position] / hyper_paramters.Sound.F0_Max
+                pattern_Path = os.path.join(
+                    hyper_paramters.Train.Train_Pattern.Path if np.random.rand() > 0.001 else hyper_paramters.Train.Eval_Pattern.Path,
+                    'NAMS',
+                    '{:03d}'.format(index),
+                    'NAMS.S_{:03d}.P_{:05d}.pickle'.format(index, pattern_Index)
+                    ).replace('\\', '/')
+                os.makedirs(os.path.dirname(pattern_Path), exist_ok= True)
+                pickle.dump(
+                    pattern,
+                    open(pattern_Path, 'wb'),
+                    protocol=4
+                    )
+                pattern_Index += 1
 
-            silence = np.where(np.mean(mel, axis=1) < -3.5, np.zeros_like(np.mean(mel, axis=1)), np.ones_like(np.mean(mel, axis=1)))
-
-            pattern_Index = 0
-            for start_Index in tqdm(range(len(music)), desc= os.path.basename(wav_Path)):
-                for end_Index in range(start_Index + 1, len(music), 3):
-                    music_Sample = music[start_Index:end_Index]
-                    sample_Length = music_Sample[-1][0] + music_Sample[-1][1] - music_Sample[0][0]
-                    if sample_Length < hyper_paramters.Min_Duration:
-                        continue
-                    elif sample_Length > hyper_paramters.Max_Duration:
-                        break
-                    
-                    audio_Sample = audio[music_Sample[0][0] * hyper_paramters.Sound.Frame_Shift:(music_Sample[-1][0] + music_Sample[-1][1]) * hyper_paramters.Sound.Frame_Shift]
-                    mel_Sample = mel[music_Sample[0][0]:music_Sample[-1][0] + music_Sample[-1][1]]
-                    silence_Sample = silence[music_Sample[0][0]:music_Sample[-1][0] + music_Sample[-1][1]]
-                    pitch_Sample = pitch[music_Sample[0][0]:music_Sample[-1][0] + music_Sample[-1][1]]
-
-                    _, duration_Sample, text_Sample, Note_Sample = zip(*music_Sample)
-
-                    pattern = {
-                        'Audio': audio_Sample.astype(np.float32),
-                        'Mel': mel_Sample.astype(np.float32),
-                        'Silence': silence_Sample.astype(np.uint8),
-                        'Pitch': pitch_Sample.astype(np.float32),
-                        'Duration': duration_Sample,
-                        'Text': text_Sample,
-                        'Note': Note_Sample,
-                        'Singer': 'Female_0', 
-                        'Dataset': 'NAMS',
-                        }
-
-                    pattern_Path = os.path.join(
-                        hyper_paramters.Train.Train_Pattern.Path if np.random.rand() > 0.001 else hyper_paramters.Train.Eval_Pattern.Path,
-                        'NAMS',
-                        '{:03d}'.format(index),
-                        'NAMS.S_{:03d}.P_{:05d}.pickle'.format(index, pattern_Index)
-                        ).replace('\\', '/')
-                    os.makedirs(os.path.dirname(pattern_Path), exist_ok= True)
-                    pickle.dump(
-                        pattern,
-                        open(pattern_Path, 'wb'),
-                        protocol=4
-                        )
-                    pattern_Index += 1
-
-                    min_Duration, max_Duration = min(sample_Length, min_Duration), max(sample_Length, max_Duration)
-            min_Note, max_Note = min(list(zip(*music))[3] + (min_Note,)), max(list(zip(*music))[3] + (max_Note,))
+                min_Duration, max_Duration = min(sample_Length, min_Duration), max(sample_Length, max_Duration)
+        min_Note, max_Note = min(list(zip(*music))[3] + (min_Note,)), max(list(zip(*music))[3] + (max_Note,))
 
     print('Duration range: {} - {}'.format(min_Duration, max_Duration))
     print('Note range: {} - {}'.format(min_Note, max_Note))
--- temp.py
+++ temp.py
@@ -31,10 +31,48 @@
 # plt.hist(notes, 41)
 # plt.show()
 
-import pickle
+# import pickle
 
-a = pickle.load(open("E:/48K.KO_Music/Train/NAMS/000/NAMS.S_000.P_00016.pickle", 'rb'))
-b = pickle.load(open("E:/48K.KO_Music/Train/NAMS/001/NAMS.S_001.P_00011.pickle", 'rb'))
+# a = pickle.load(open("E:/48K.KO_Music/Train/NAMS/000/NAMS.S_000.P_00016.pickle", 'rb'))
+# b = pickle.load(open("E:/48K.KO_Music/Train/NAMS/001/NAMS.S_001.P_00011.pickle", 'rb'))
 
-list(zip(a['Text'], a['Note']))
-list(zip(b['Text'], b['Note']))
+# list(zip(a['Text'], a['Note']))
+# list(zip(b['Text'], b['Note']))
+
+
+
+
+# import mido, os, pickle, yaml, hgtk, argparse, math
+
+# for root, _, files in os.walk('C:/Users/Heejo/Desktop/작업 파일/'):
+#     for file in files:
+#         if os.path.splitext(file)[1] != '.mid':
+#             continue
+#         path = os.path.join(root, file).replace('//', '/')
+#         mid = mido.MidiFile(path, charset='CP949')
+#         for message in mid:
+#             if message.type == 'lyrics' and (not hgtk.checker.is_hangul(message.text.strip()) or message.text.strip() == ''):
+#                 print(file)
+#                 break
+                
+
+
+
+import mido, os, pickle, yaml, hgtk, argparse, math
+
+# for root, _, files in os.walk('C:/Users/Heejo/Desktop/작업 파일/'):
+#     for file in files:
+#         if os.path.splitext(file)[1] != '.mid':
+#             continue
+#         wav_Path = os.path.join(root, file).replace('//', '/')
+#         midi_Path = wav_Path.replace('Vox.wav', 'Midi.mid')
+#         paths.append((wav_Path, midi_Path))
+
+mid = mido.MidiFile('C:/Users/Heejo/Desktop/작업 파일/76. 00488 물들어 MIDI.mid', charset='CP949')
+music = []
+current_Lyric = ''
+previous_Used = 0
+absolute_Position = 0
+for message in mid:
+    if message.type == 'lyrics':
+        print(message)
